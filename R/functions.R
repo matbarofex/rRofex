629fdf6db7df57c16527261c9741bddc7613c12c
Augusto Hassel <augusto.hassel@gmail.com>
2020-05-05 18:54:53 -0300

    Adapted all trading functions to respond with tibbles

--- R/functions.R
+++ R/functions.R
@@ -553,31 +553,34 @@ trading_currencies <- function(connection) {
 #' @description The method \code{trading_new_order} is use to send orders.
 #'
 #' @param connection S4. \strong{Mandaroty} Formal rRofexConnection class object
+#' @param account String. \strong{Mandaroty} Account Number
 #' @param symbol String. Use \code{\link{trading_instruments}} to see which symbols are available.
-#' @param side String. Either 'Buy' or 'Sell'.
-#' @param quantity Numeric. Quantity of the order.
-#' @param price Numeric. Price of the order.
+#' @param side String. \strong{Mandaroty} Either:
+#' \itemize{
+#' \item \strong{Buy}
+#' \item \strong{Sell}
+#' }
+#' @param quantity Numeric. \strong{Mandaroty} Quantity of the order.
+#' @param price Numeric. \strong{Mandaroty} Price of the order.
 #' @param order_type String. Type of order.
 #' \itemize{
-#' \item Limit. Limit order sets the maximum or minimum price at which you are willing to buy or sell.
-#' \item MLL. Market with Leftover as Limit (market order then unexecuted quantity becomes limit order at last price).
+#' \item \strong{Limit} - Default. Limit order sets the maximum or minimum price at which you are willing to buy or sell.
 #' }
 #' @param time_in_force String. Specifies how long the order remains in effect. Absence of this field is interpreted as 'Day':
 #' \itemize{
-#' \item Day. Day or session.
-#' \item IOC. Immediate or Cancel.
-#' \item FOK. Fill or Kill.
-#' \item GTD. Good Till Date.
+#' \item \strong{Day} - Day or session.
+#' \item \strong{IOC} - Immediate or Cancel.
+#' \item \strong{FOK} - Fill or Kill.
+#' \item \strong{GTD} - Good Till Date.
 #' }
-#' @param iceberg Logical: if TRUE, then the order is 'iceberg'. FALSE as default.
+#' @param iceberg Logical. If TRUE, then the order is 'iceberg'. FALSE as default.
 #' @param expire_date String. \strong{Only for GDT orders}. Maturity date of the order, With format '\%Y-\%m-\%d'.
 #' @param display_quantity Numeric. \strong{Only for Iceberg orders}. Indicate the disclosed quantity for the 'iceberg' order.
-#' @param account String. Account Number / Account ID.
 #'
-#' @return List with outputs like state of the order.
+#' @return If correct, it will load a tibble.
 #'
 #' @family order placements functions
-trading_new_order <- function(connection, symbol, side, quantity, price, order_type='Limit', time_in_force='Day', iceberg=FALSE, expire_date=NULL, display_quantity=NULL, account) {
+trading_new_order <- function(connection, account, symbol, side, quantity, price, order_type='Limit', time_in_force='Day', iceberg=FALSE, expire_date=NULL, display_quantity=NULL) {
 
   if (missing(connection)) stop("Connection cannot be empty.")
   if (!isS4(connection) || rev(class(connection)) != "rRofexConnection" || !validObject(connection)) stop("The 'connection' must be a valid 'rRofexConnection'.")
@@ -593,10 +596,8 @@ trading_new_order <- function(connection, symbol, side, quantity, price, order_t
 
   if (missing(quantity)) stop("You should pick a 'quantity' to move forward.")
 
+  if (!order_type %in% c("Limit")) stop("Invalid 'order_type' parameter")
   if (order_type == "Limit" & missing(price)) stop("You should pick a 'price' to move forward.")
-  if (order_type == "MLL" & missing(price)) {price <- ""}
-
-  if (!order_type %in% c("Limit", "MLL")) stop("Invalid 'order_type' parameter")
 
   if (!time_in_force %in% c("Day", "IOC", "FOK", "GTD")) stop("Invalid 'time_in_force' parameter")
 
@@ -609,18 +610,17 @@ trading_new_order <- function(connection, symbol, side, quantity, price, order_t
 
   if (iceberg == "TRUE" & missing(display_quantity)) stop("You should provide a disclosed quantity")
 
-
-  if (missing(account)) stop("You should pick a 'account' to move forward.")
+  if (missing(account)) stop("'account' parameter cannot be empty.")
 
   # Query
-  query <- GET(url = paste0(connection@base_url, "/rest/order/newSingleOrder"),
+  query <- GET(url = glue(connection@base_url, "/rest/order/newSingleOrder"),
                query = list(
                  marketId    = market_id,
                  symbol      = symbol,
                  side        = side,
                  orderQty    = quantity,
                  price       = price,
-                 ordType     = if (order_type == "Limit") {"Limit"} else if (order_type == "MLL") {"Market_to_limit"},
+                 ordType     = order_type,
                  timeInForce = time_in_force,
                  iceberg     = iceberg,
                  expireDate  = expire_date,
@@ -629,56 +629,97 @@ trading_new_order <- function(connection, symbol, side, quantity, price, order_t
                ),
                add_headers(.headers = c("X-Auth-Token" = connection@token)))
 
-  if (query$status_code != 200 | content(query)$status != "OK") stop("The query returned an unexpected result.")
+  if (status_code(query) != 200) {
 
-  # Result
-  result <- if (content(query)$status == "OK") {
-    trading_lookup(lookup_type = "COID",
-                   order_id = content(query)$order$clientId,
-                   proprietary =content(query)$order$proprietary)
+    warn_for_status(query)
+    message("\r")
+    data <- NULL
+
+  } else if  (content(query)$status != "OK") {
+
+    message(glue("Something wrong happend when placing the order.
+
+                 More info: {content(query)$status}"))
+    data <- NULL
+
   } else {
-    content(query)
+
+    data <- fromJSON(toJSON(content(query), auto_unbox = T, null = "null"), simplifyDataFrame = T)
+    data <- data$order %>%
+      as_tibble()
+
   }
 
-  return(result)
+  if (!is_null(data)) {
+    data <- trading_lookup(connection = connection,
+                   lookup_type = "COID",
+                   id = data$clientId,
+                   proprietary = data$proprietary)
+  }
+
+  return(data)
 }
 
 #' @title Cancel Order Sent to the Market
 #'
-#' @description The method \code{trading_cancel_order} is use to send orders.
+#' @description The method \code{trading_cancel_order} should be use to cancel orders that are open on the market.
 #'
 #' @param connection S4. \strong{Mandaroty} Formal rRofexConnection class object
-#' @param order_id String. clOrdId given by the \code{trading_orders} method.
-#' @param proprietary String. ID given by the \code{trading_orders} method.
+#' @param id String. \strong{Mandaroty} clOrdId given by the \code{trading_orders} method.
+#' @param proprietary String. \strong{Mandaroty} ID given by the \code{trading_orders} method.
+#' \itemize{
+#' \item \strong{PBCP}
+#' }
 #'
-#' @return List with outputs like state of the order.
+#' @return If correct, it will load a tibble.
 #'
 #' @family order placements functions
-trading_cancel_order <- function(connection, order_id, proprietary) {
+trading_cancel_order <- function(connection, id, proprietary) {
 
   if (missing(connection)) stop("Connection cannot be empty.")
   if (!isS4(connection) || rev(class(connection)) != "rRofexConnection" || !validObject(connection)) stop("The 'connection' must be a valid 'rRofexConnection'.")
   if (as.Date(connection@login_date_time) != Sys.Date()) stop("The 'acyRsaConnection' is no longer valid. Please log-in again.")
 
-  if (missing(order_id)) stop("You should pick a 'order_id' to move forward.")
+  if (missing(id)) stop("You should pick a 'id' to move forward.")
   if (missing(proprietary)) stop("You should pick a 'proprietary' to move forward.")
 
   # Query
-  query <- GET(url = paste0(connection@base_url, "/rest/order/cancelById"),
+  query <- GET(url = glue(connection@base_url, "/rest/order/cancelById"),
                query = list(
-                 clOrdId     = order_id,
+                 clOrdId     = id,
                  proprietary = proprietary
                ),
                add_headers(.headers = c("X-Auth-Token" = connection@token)))
 
-  # Results
-  if (query$status_code != 200 | content(query)$status != "OK") stop("The query returned an unexpected result.")
+  if (status_code(query) != 200) {
 
-  result <- if (query$status_code == 200 & content(query)$status == "OK") {
-    "The order has been canceled!"
+    warn_for_status(query)
+    message("\r")
+    data <- NULL
+
+  } else if  (content(query)$status != "OK") {
+
+    message(glue("Something wrong happend when placing the order.
+
+                 More info: {content(query)$status}"))
+    data <- NULL
+
+  } else {
+
+    data <- fromJSON(toJSON(content(query), auto_unbox = T, null = "null"), simplifyDataFrame = T)
+    data <- data$order %>%
+      as_tibble()
+
   }
 
-  return(result)
+  if (!is_null(data)) {
+    data <- trading_lookup(connection = connection,
+                           lookup_type = "COID",
+                           id = data$clientId,
+                           proprietary = data$proprietary)
+  }
+
+  return(data)
 }
 
 # Orders Lookup ---------------------------
@@ -688,18 +729,21 @@ trading_cancel_order <- function(connection, order_id, proprietary) {
 #' @description The method \code{trading_lookup} is used to check the satus of an order.
 #'
 #' @param connection S4. \strong{Mandaroty} Formal rRofexConnection class object
-#' @param lookup_type String. Look-up by:
+#' @param lookup_type String. \strong{Mandaroty}. Look-up by:
 #' \itemize{
-#' \item COID. Client Order ID.
-#' \item OID. Order ID. (Not Available)
+#' \item \strong{COID} - Client Order ID.
+#' \item \strong{OID} - Order ID.
 #' }
-#' @param order_id String. ID given by the \code{trading_orders} method.
-#' @param proprietary String. ID given by the \code{trading_orders} method.
+#' @param id String. \strong{Mandaroty}. ID given by the \code{trading_orders} method. Depends on `lookup_type`.
+#' @param proprietary String. ID given by the \code{trading_orders} method. Only for 'lookup_type=COID' In most cases:
+#' \itemize{
+#' \item \strong{PBCP}
+#' }
 #'
-#' @return A data frame.
+#' @return If correct, it will load a tibble.
 #'
 #' @family order management functions
-trading_lookup <- function(connection, lookup_type, order_id, proprietary) {
+trading_lookup <- function(connection, lookup_type, id, proprietary) {
 
   if (missing(connection)) stop("Connection cannot be empty.")
   if (!isS4(connection) || rev(class(connection)) != "rRofexConnection" || !validObject(connection)) stop("The 'connection' must be a valid 'rRofexConnection'.")
@@ -708,40 +752,63 @@ trading_lookup <- function(connection, lookup_type, order_id, proprietary) {
   if (missing(lookup_type)) stop("You should pick a 'lookup_type' to move forward.")
   if (!lookup_type %in% c("COID", "OID")) stop("Invalid 'lookup_type' parameter")
 
-  if (missing(order_id)) stop("You should pick a 'order_id' to move forward.")
+  if (missing(id)) stop("You should pick a 'id' to move forward.")
 
   if (lookup_type == "COID" & missing(proprietary)) stop("You should pick a 'proprietary' to move forward.")
 
   # Query
   query <- if (lookup_type == "COID") {
-    GET(url = paste0(connection@base_url, "/rest/order/id"),
+    GET(url = glue(connection@base_url, "/rest/order/id"),
         query = list(
-          clOrdId     = order_id,
+          clOrdId     = id,
           proprietary = proprietary
         ),
         add_headers(.headers = c("X-Auth-Token" = connection@token)))
   } else if (lookup_type == "OID") {
-    # paste0(.rRofexGlobalEnv$base_url, "/rest/order/cancelById")
+    GET(url = glue(connection@base_url, "/rest/order/byOrderId"),
+        query = list(
+          orderId     = id
+        ),
+        add_headers(.headers = c("X-Auth-Token" = connection@token)))
   }
 
-  # Results
-  if (query$status_code != 200 | content(query)$status != "OK") stop("The query returned an unexpected result.")
+  if (status_code(query) != 200) {
 
-  result <- if (query$status_code == 200 & content(query)$status == "OK") {
-    list(
-      ClientId   = content(query)$order$clOrdId,
-      State      = content(query)$order$status,
-      text       = content(query)$order$text
-    )
+    warn_for_status(query)
+    message("\r")
+    data <- NULL
+
+  } else if  (content(query)$status != "OK") {
+
+    message(glue('Something wrong happend when placing the order.\n',
+                 'Info: {message}\n',
+                 '{description}',
+                 message = ifelse(is_null(content(query)$message), "<empty>", content(query)$message),
+                 description = content(query)$description))
+    data <- NULL
+
   } else {
-    list(
-      ClientId   = "-",
-      State      = "-",
-      text       = "-"
-    )
+
+    data <- fromJSON(toJSON(content(query), auto_unbox = T, null = "null"), simplifyDataFrame = T)
+
+    if (is_empty(data$order)) {
+
+      message(glue('No orders placed at the moment.\n'))
+      data <- NULL
+
+    } else {
+      data <- data$order %>%
+        unlist(recursive = F) %>%
+        as_tibble() %>%
+        rename_all(., .funs = ~ gsub(pattern = "\\.", replacement = "_", x = .)) %>%
+        mutate(
+          transactTime = as.POSIXct(transactTime, format = "%Y%m%d-%H:%M:%OS", tz = "America/Buenos_Aires"),
+          text = trimws(text, which = "both")
+        )
+    }
   }
 
-  return(result)
+  return(data)
 }
 
 #' @title View Orders
@@ -749,9 +816,9 @@ trading_lookup <- function(connection, lookup_type, order_id, proprietary) {
 #' @description The method \code{trading_orders} is used to see each order sent by Account.
 #'
 #' @param connection S4. \strong{Mandaroty} Formal rRofexConnection class object
-#' @param account String. Account Number / Account ID.
+#' @param account String. \strong{Mandaroty} Account Number
 #'
-#' @return A data frame.
+#' @return If correct, it will load a tibble.
 #'
 #' @family order management functions
 trading_orders <- function(connection, account) {
@@ -760,23 +827,53 @@ trading_orders <- function(connection, account) {
   if (!isS4(connection) || rev(class(connection)) != "rRofexConnection" || !validObject(connection)) stop("The 'connection' must be a valid 'rRofexConnection'.")
   if (as.Date(connection@login_date_time) != Sys.Date()) stop("The 'acyRsaConnection' is no longer valid. Please log-in again.")
 
-  if (missing(account)) stop("You should pick an 'account' to move forward.")
+  if (missing(account)) stop("'account' parameter cannot be empty.")
 
   # Query
-  query <- GET(url = paste0(connection@base_url, "/rest/order/all"),
+  query <- GET(url = glue(connection@base_url, "/rest/order/all"),
                query = list(
                  accountId = account
                ),
                add_headers(.headers = c("X-Auth-Token" = connection@token)))
 
-  # Results
-  if (query$status_code != 200 | content(query)$status != "OK") stop("The query returned an unexpected result.")
 
-  result <- if (query$status_code == 200 & content(query)$status == "OK") {
-    fromJSON(content(x = query, as = "text"))
+  if (status_code(query) != 200) {
+
+    warn_for_status(query)
+    message("\r")
+    data <- NULL
+
+  } else if  (content(query)$status != "OK") {
+
+    message(glue('Something wrong happend when placing the order.\n',
+                 'Info: {message}\n',
+                 '{description}',
+                 message = content(query)$message,
+                 description = content(query)$description))
+    data <- NULL
+
+  } else {
+
+    data <- fromJSON(toJSON(content(query), auto_unbox = T, null = "null"), simplifyDataFrame = T)
+
+    if (is_empty(data$orders)) {
+
+      message(glue('No orders placed at the moment.\n'))
+      data <- NULL
+
+    } else {
+      data <- data$orders %>%
+        jsonlite::flatten(., recursive = F) %>%
+        as_tibble() %>%
+        rename_all(., .funs = ~ gsub(pattern = "\\.", replacement = "_", x = .)) %>%
+        mutate(
+          transactTime = as.POSIXct(transactTime, format = "%Y%m%d-%H:%M:%OS", tz = "America/Buenos_Aires"),
+          text = trimws(text, which = "both")
+        )
+    }
   }
 
-  return(result$orders)
+  return(data)
 }
 
 # Account Information ---------------------------
